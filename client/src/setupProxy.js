38b50a84fd7f6c4f4b9e6b7949473c188d88bb21
Jacqueline Guo <jacquelineguo@Jacquelines-MacBook-Pro.local>
2022-04-26 18:30:19 -0700

    update aws dns

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
     ["/api", "/auth/google"],
     createProxyMiddleware({
      // target: "http://host.docker.internal:5000",
-     target: "http://localhost:4000",
-//       target: "http://ec2-3-235-247-145.compute-1.amazonaws.com:5000",
+     // target: "http://localhost:4000",
+      target: "http://ec2-44-203-242-177.compute-1.amazonaws.com:5000",
 
 
       secure: false,
